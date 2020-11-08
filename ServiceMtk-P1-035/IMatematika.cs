6ff7303719e03b0a6d50dea4ed1309688ef1aaad
poetra-As <62783856+poetra-As@users.noreply.github.com>
2020-11-08 22:33:43 +0700

    WCF EXCEPTION HANDLING

--- ServiceMtk-P1-035/IMatematika.cs
+++ ServiceMtk-P1-035/IMatematika.cs
@@ -7,20 +7,35 @@ using System.Text;
 
 namespace ServiceMtk_P1_035
 {
+    //Tambahkan kelas berikut
+    [DataContract]
+    class MathFault
+    { [DataMember]
+      public string Kode { get; set; }
+       
+        [DataMember]
+      public string Pesan { get; set; }
+        
+    }
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IMatematika
     {
         [OperationContract]
         /* output->*/
+        [FaultContract(typeof(MathFault))]//membuat kelas yang belum yang melambangkan exception
         int Tambah(int a, int b); //method
         [OperationContract]
+        [FaultContract(typeof(MathFault))]//membuat kelas yang belum yang melambangkan exception
         int Kurang(int a, int b); //input
         [OperationContract]
+        [FaultContract(typeof(MathFault))]//membuat kelas yang belum yang melambangkan exception
         int Kali(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]//membuat kelas yang belum yang melambangkan exception
         int Bagi(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]//membuat kelas yang belum yang melambangkan exception
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
     }
 
--- ServiceMtk-P1-035/Matematika.cs
+++ ServiceMtk-P1-035/Matematika.cs
@@ -10,32 +10,101 @@ namespace ServiceMtk_P1_035
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Matematika : IMatematika
     {
+        
         public int Bagi(int a, int b)
         {
-            return a / b;
+            try
+            {
+                return a / b;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda massukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+
         }
+
 
         public int Kali(int a, int b)
         {
-            return a * b;
+            try
+            {
+                return a * b;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda massukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
         public int Kurang(int a, int b)
         {
-            return a - b;
+            try
+            {
+                return a - b;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda massukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
         public int Tambah(int a, int b)
         {
-            return a + b;
+            try
+            {
+                return a + b;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda massukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
         public Koordinat TKoordinat(Koordinat a, Koordinat b)
         {
-            Koordinat hasil = new Koordinat();
-            hasil.X = a.X + b.X;
-            hasil.Y = a.Y + b.Y;
-            return hasil;
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda massukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
     }
-}
+    }
+
